60b4d1828e9f1d867b46da800719d803810f5ab6
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-12-21 00:54:46 +0300

    task 6

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+# 2-square.py
+"""Define a class Square."""
+
+
 class Square:
-    """
-    creates a square object
-    """
+    """Represent a square."""
+
     def __init__(self, size=0):
-        if(type(size) is not int):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if(size < 0):
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        """
-        initializes instance of a square
-        Args:
-            __size(int): size of square
-            size must be positive and integer type
-        """
