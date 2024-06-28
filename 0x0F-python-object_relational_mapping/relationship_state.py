e294ada9d4796c492e329275cec393a74fadf1e7
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2024-06-29 01:08:08 +0300

    solving all issue in base code

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
+"""
+This script defines a City class
+to work with MySQLAlchemy ORM.
+"""
+
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class State(Base):
-    """Represents a state for a MySQL database.
-
+class City(Base):
+    """City class
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-        cities (sqlalchemy.orm.relationship): The State-City relationship.
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
     """
-    __tablename__ = "states"
+    __tablename__ = 'cities'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
