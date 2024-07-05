21119d01cf2ea383390c713fac98eb69eded1736
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2024-07-06 00:01:41 +0300

    later changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-# fetches website
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
+
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        content = response.read()
-        url = content.decode('utf-8')
-        typed = response.info()
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(url))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
